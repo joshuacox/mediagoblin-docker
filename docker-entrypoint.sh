377d5291e620c45593fd485d59c538bd1b07ad56
Josh Cox <josh@webhosting.coop>
2017-07-01 14:43:31 -0500

    configured

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,23 @@
 nginx
 chown -hR mediagoblin:www-data /var/lib/mediagoblin
 cd /srv/mediagoblin.example.org/mediagoblin/
-sudo -u mediagoblin bin/gmg dbupdate
-sudo -u mediagoblin bin/gmg adduser --username admin --password admin --email some@where.com
-sudo -u mediagoblin bin/gmg makeadmin admin
-sudo -u mediagoblin ./lazyserver.sh --server-name=fcgi fcgi_host=127.0.0.1 fcgi_port=26543
+if [ ! -f /var/lib/mediagoblin/date-configured.txt ]
+  then
+    if [ -z ${GOBLIN_USER+x} ]
+      then
+        echo set GOBLIN_USER
+    fi
+    if [ -z ${GOBLIN_PASSWORD+x} ]
+      then
+        echo set GOBLIN_PASSWORD
+    fi
+    if [ -z ${GOBLIN_EMAIL+x} ]
+      then
+        echo set GOBLIN_EMAIL
+    fi
+    sudo -u mediagoblin bin/gmg dbupdate
+    sudo -u mediagoblin bin/gmg adduser --username $GOBLIN_USER --password $GOBLIN_PASSWORD --email $GOBLIN_EMAIL
+    sudo -u mediagoblin bin/gmg makeadmin $GOBLIN_USER
+    sudo -u mediagoblin ./lazyserver.sh --server-name=fcgi fcgi_host=127.0.0.1 fcgi_port=26543
+    date -I > /var/lib/mediagoblin/date-configured.txt
+fi
